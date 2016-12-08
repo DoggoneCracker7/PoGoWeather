b45e61e998650cb644828c126cbf21e2af8e9eb7
mfundakowski <mateuszfundakowski@gmail.com>
2016-12-08 22:14:06 +0100

    Added fragment for Main Activity

--- app/src/main/java/com/example/kbaran/pogoweather/MainActivity.java
+++ app/src/main/java/com/example/kbaran/pogoweather/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.kbaran.pogoweather;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WeatherFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayWeatherFragment();
+    }
+
+    public void displayWeatherFragment() {
+        WeatherFragment fragment = new WeatherFragment();
+        fragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
