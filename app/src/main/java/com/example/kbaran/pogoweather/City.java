4e86d65fead04f99e1f0b72827b4ca2cbf023b45
konrad <condi110@gmail.com>
2016-12-08 23:46:55 +0100

    modele

---
+++ app/src/main/java/com/example/kbaran/pogoweather/City.java
@@ -0,0 +1,16 @@
+package com.example.kbaran.pogoweather;
+
+
+public class City {
+    private int cityId;
+
+    public City(){}
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/example/kbaran/pogoweather/Location.java
@@ -0,0 +1,66 @@
+package com.example.kbaran.pogoweather;
+
+
+import java.util.Set;
+
+public class Location {
+    private String cityName;
+    private float cityCoordLatitude;
+    private float cityCoordLongitude;
+    private String cityCountry;
+    private Set<Weather> weather;
+
+    public Location(){}
+
+    public Location(String cityName, int cityCoordLatitude, int cityCoordLongitude, String cityCountry){
+        this.cityName = cityName;
+        this.cityCoordLatitude = cityCoordLatitude;
+        this.cityCoordLongitude = cityCoordLongitude;
+        this.cityCountry = cityCountry;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public float getCityCoordLatitude() {
+        return cityCoordLatitude;
+    }
+
+    public void setCityCoordLatitude(float cityCoordLatitude) {
+        this.cityCoordLatitude = cityCoordLatitude;
+    }
+
+    public float getCityCoordLongitude() {
+        return cityCoordLongitude;
+    }
+
+    public void setCityCoordLongitude(float cityCoordLongitude) {
+        this.cityCoordLongitude = cityCoordLongitude;
+    }
+
+    public String getCityCountry() {
+        return cityCountry;
+    }
+
+    public void setCityCountry(String cityCountry) {
+        this.cityCountry = cityCountry;
+    }
+
+    public Set<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Set<Weather> weather) {
+        this.weather = weather;
+    }
+
+    @Override
+    public String toString() {
+        return this.cityName;
+    }
+}
